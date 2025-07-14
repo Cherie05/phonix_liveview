fa9378728a6899917c62e9e3598a33ee388ffa55
Arun <arun@Aruns-MacBook-Pro.local>
2025-07-14 16:54:48 +0530

    fixed accounts

--- liveview/lib/liveview_web/live/navbar_live.ex
+++ liveview/lib/liveview_web/live/navbar_live.ex
@@ -1,8 +1,34 @@
 defmodule LiveviewWeb.NavbarLive do
   use LiveviewWeb, :live_view
+  alias Liveview.Accounts
 
   # will compile any *.html.heex under navbar_live_html/
   embed_templates "templates/*"
+
+
+  @impl true
+  def mount(_params, session, socket) do
+    socket =
+      socket
+      |> assign(:menu_open, false)
+      |> assign_current_user(session)
+
+    {:ok, socket}
+  end
+
+  defp assign_current_user(socket, %{"user_id" => id}) do
+    case Accounts.get_user(id) do
+      nil   -> socket
+      user  -> assign(socket, :current_user, user)
+    end
+  end
+  defp assign_current_user(socket, _), do: socket
+
+  @impl true
+  def handle_event("toggle_menu", _params, socket) do
+    {:noreply, update(socket, :menu_open, &(!&1))}
+  end
+
 
   @impl true
   def mount(_params, _session, socket) do
